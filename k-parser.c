c4a3052a6d7a77e91ba6c29f675f6a432e62b2d4
Kelvin <kelvingremista2015@hotmail.com>
2025-10-11 19:42:24 -0300

    created buffer to deal with strings

--- k-parser.c
+++ k-parser.c
@@ -3,10 +3,19 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+#define BUFFER_SIZE 50
+
+void empty_buff(char buffer[BUFFER_SIZE]){
+    for (int i = 0; i < BUFFER_SIZE; i++)
+    {
+       buffer[i] = '-';
+    }
+    buffer[BUFFER_SIZE] = '\0';
+}
+
 int main()
 {
     FILE *fptr;
-    int c;
     fptr = fopen("schema.prisma", "r");
     if (fptr == NULL)
     {
@@ -14,10 +23,14 @@ int main()
         fclose(fptr);
         return 1;
     }
+    char buffer[BUFFER_SIZE];
+    empty_buff(buffer);
+    int c;
     int i = 0;
     int l = 0;
     int ip = 0;
     int in_word = 0;
+    int bs = 0;
     while ((c = getc(fptr)) != EOF)
     {
         if (!isspace((char)c))
@@ -27,14 +40,23 @@ int main()
                 ip = i;
             }
             in_word = 1;
+            if (bs >= BUFFER_SIZE)
+            {
+                printf("buffer exceed");
+                return 1;
+            }
+            buffer[bs] = (char)c;
+            bs++;
             // Debug
             printf("%c", (char)c);
         }
         else if (in_word)
         {
+            bs = 0;
             in_word = 0;
             // Debug
             printf(" ----- init: %d, end: %d, line: %d\n", ip, i, l);
+            printf("buffer: %s\n", buffer);
         }
         if ((char)c == '\n')
         {
@@ -42,7 +64,6 @@ int main()
         }
         i++;
     }
-
     fclose(fptr);
     return 0;
 }
