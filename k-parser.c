d43a791bb64cd942fbc745f130af578ad30149d2
Kelvin <kelvingremista2015@hotmail.com>
2025-10-14 00:33:24 -0300

    feat: lexer

--- k-parser.c
+++ k-parser.c
@@ -5,11 +5,6 @@
 
 #define BUFFER_SIZE 50
 
-// struct Table
-// {
-//     char *table_name;
-// } Table_t;
-
 void empty_buff(char buffer[BUFFER_SIZE])
 {
     for (int i = 0; i < BUFFER_SIZE; i++)
@@ -37,9 +32,6 @@ int main()
     int initial_position = 0;
     int in_word = 0;
     int buffer_size = 0;
-    int is_table = 0;
-    int is_column = 0;
-    int is_param = 0;
     while ((current_char = getc(fptr)) != EOF)
     {
         if (!isspace((char)current_char))
@@ -57,57 +49,22 @@ int main()
             buffer[buffer_size] = (char)current_char;
             buffer_size++;
             // Debug
-            printf("%c", (char)current_char);
+            // printf("%c", (char)current_char);
         }
         else if (in_word)
         {
             buffer_size = 0;
             in_word = 0;
-            // Debug
-            printf(" ----- init: %d, end: %d, line: %d\n", initial_position, i, line);
-            // Debug
-            printf("total: %d, buffer: %s\n", i - initial_position, buffer);
-            int length = i - initial_position;
-            char cmp[length];
-            for (int i = 0; i < length; i++)
-            {
-                cmp[i] = buffer[i];
-            }
-            cmp[length] = '\0';
-            if (strcmp("}", cmp) == 0)
-            {
-                is_column = 0;
-            }
-            if (is_param)
-            {
-                // debug: column param
-                printf("column param: %s\n", cmp);
-            }
-            else if (is_column && strcmp("{", cmp) == 1)
-            {
-                // debug: column name
-                printf("column name: %s\n", cmp);
-                is_param = 1;
-            }
-            else if (is_table)
-            {
-                // debug: table name
-                printf("table name: %s\n", cmp);
-                is_table = 0;
-                is_column = 1;
-            }
-            else if (strcmp("table", cmp) == 0 && !is_table)
-            {
-                is_table = 1;
-            }
+            int total_length = i - initial_position;
+            printf("%d\n", total_length);
         }
         if ((char)current_char == '\n')
         {
             line++;
-            is_param = 0;
         }
         i++;
     }
+    printf("%d\n", line);
     fclose(fptr);
     return 0;
 }
