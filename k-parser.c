22cf44401b310411e7439708c800864cd4fbdb8d
Kelvin <kelvingremista2015@hotmail.com>
2025-10-15 01:07:06 -0300

    update schema.prisma and lex

--- k-parser.c
+++ k-parser.c
@@ -12,6 +12,7 @@ typedef enum
 
 typedef struct TOKEN
 {
+    char *value;
     int start;
     int line;
     KTOKENS type;
@@ -20,10 +21,26 @@ typedef struct TOKEN
 typedef struct TOKENS
 {
     TOKEN_T *tokens;
-    int tokens_count;
+    size_t tokens_count;
+    size_t capacity;
 } TOKENS_T;
 
 #define BUFFER_SIZE 50
+
+const char *token_to_string(KTOKENS token)
+{
+    switch (token)
+    {
+    case KEYWORD_TABLE:
+        return "KEYWORD_TABLE";
+    case OPENING_BRACE:
+        return "OPENING_BRACE";
+    case CLOSING_BRACE:
+        return "CLOSING_BRACE";
+    default:
+        "UNKNOWN";
+    }
+}
 
 void empty_buff(char buffer[BUFFER_SIZE])
 {
@@ -36,13 +53,57 @@ void empty_buff(char buffer[BUFFER_SIZE])
 
 void init_tokens(TOKENS_T *ptr_token, size_t initial_capacity)
 {
+
     ptr_token->tokens = (TOKEN_T *)malloc(sizeof(TOKEN_T) * initial_capacity);
+    if (ptr_token->tokens == NULL)
+    {
+        printf("Failed to allocate memory");
+        exit(1);
+    }
     ptr_token->tokens_count = 0;
+    ptr_token->capacity = initial_capacity;
+}
+
+void add_token(TOKENS_T *ptr_token, TOKEN_T token)
+{
+    if (ptr_token->capacity == ptr_token->tokens_count)
+    {
+        // TODO: realloc memory
+        printf("\ncannot added token, limit reached\n");
+        exit(1);
+    }
+    else
+    {
+        ptr_token->tokens[ptr_token->tokens_count] = token;
+        ptr_token->tokens_count++;
+    }
 }
 
 void free_tokens(TOKENS_T *ptr_token)
 {
     free(ptr_token->tokens);
+}
+
+void tokenize_word(TOKENS_T *tokens, char *word, int initial_position, int line)
+{
+    if (strcmp("table", word) == 0)
+    {
+        TOKEN_T token = {.line = line, .start = initial_position, .type = KEYWORD_TABLE};
+        add_token(tokens, token);
+        // free(word);
+    }
+    if (strcmp("{", word) == 0)
+    {
+        TOKEN_T token = {.line = line, .start = initial_position, .type = OPENING_BRACE};
+        add_token(tokens, token);
+    }
+    if (strcmp("}", word) == 0)
+    {
+        TOKEN_T token = {.line = line, .start = initial_position, .type = CLOSING_BRACE};
+        add_token(tokens, token);
+    }
+    // need free only when the token not have a value, like KEYWORDS,OPENING_BRACE etc..
+    free(word);
 }
 
 int main()
@@ -82,19 +143,24 @@ int main()
             }
             buffer[buffer_size] = (char)current_char;
             buffer_size++;
-            // Debug
-            // printf("%c", (char)current_char);
         }
         else if (in_word)
         {
             buffer_size = 0;
             in_word = 0;
             int total_length = i - initial_position;
+            char *cmp_buff = malloc(sizeof(char) * total_length);
+            if (cmp_buff == NULL)
+            {
+                printf("Failed to allocate memory\n");
+                exit(1);
+            }
             for (int i = 0; i < total_length; i++)
             {
-                printf("%c", buffer[i]);
+                cmp_buff[i] = buffer[i];
             }
-            printf("\n");
+            cmp_buff[total_length] = '\0';
+            tokenize_word(&tokens, cmp_buff, initial_position, line);
         }
         if ((char)current_char == '\n')
         {
@@ -103,6 +169,11 @@ int main()
         i++;
     }
     fclose(fptr);
+    // debug
+    for (int i = 0; i < tokens.tokens_count; i++)
+    {
+        printf("value: %s, start: %d, line: %d, type: %s\n", tokens.tokens[i].value, tokens.tokens[i].start, tokens.tokens[i].line, token_to_string(tokens.tokens[i].type));
+    }
     free_tokens(&tokens);
     return 0;
 }
